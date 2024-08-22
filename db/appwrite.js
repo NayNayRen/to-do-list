d83cfe6bd3275d57cb148600c51e7dbd23d7cbe0
Nate <jeditendencies35@gmail.com>
2024-08-22 15:25:05 -0400

    adding appwrite config stuff again

---
+++ db/appwrite.js
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+	endpoint: "https://cloud.appwrite.io/v1",
+	platform: "com.jeditendencies.todoslist",
+	projectId: "66c774b50019fbe460cc",
+	databaseId: "66c776990029034067a9",
+	userCollectionId: "66c77705002a28f97253",
+	todoCollectionId: "66c777c9003ae939f262",
+};
